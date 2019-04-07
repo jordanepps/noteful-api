637b9130661c3b6c266c0e77a458776c1e65657c
Jordan Epps <jordanepps@gmail.com>
2019-04-07 00:28:49 -0400

    Update config for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'deployment',
-	DB_URL: process.env.DB_URL || 'postgresql://jordanepps@localhost/noteful'
+	DB_URL:
+		process.env.DATABASE_URL || 'postgresql://jordanepps@localhost/noteful'
 };
